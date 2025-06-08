0e98f3e012444b9d7def3a333e0dc7a6704905e7
Jon Schulte <jonlouis.schulte@agentur-david.de>
2025-06-08 16:41:20 +0200

    status bar

--- src/components/TaskBar.vue
+++ src/components/TaskBar.vue
@@ -1,7 +1,20 @@
 <template>
-    <div class="window" role="tabpanel">
+    <div class="window">
         <div class="window-body">
-            <p>the task bar</p>
+              <div class="status-bar ">
+                <p class="status-bar-field">16:37</p>
+            </div>
         </div>
   </div>
 </template>
+
+<style scoped>
+    .window-body {
+        margin: 2px;
+        display: flex;
+        justify-content: end;
+    }
+    .status-bar-field {
+        padding: 5px;
+    }
+</style>
--- src/components/Window.vue
+++ src/components/Window.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="window" style="width: 300px">
+    <div class="window" style="width: 60vw">
     <div class="title-bar">
-        <div class="title-bar-text">A Window With Stuff In It</div>
+        <div class="title-bar-text">A Window</div>
         <div class="title-bar-controls">
         <button aria-label="Minimize"></button>
         <button aria-label="Maximize"></button>
@@ -13,11 +13,3 @@
     </div>
     </div>
 </template>
-
-<script setup>
-// Add any logic here if needed
-</script>
-
-<style>
-
-</style>
