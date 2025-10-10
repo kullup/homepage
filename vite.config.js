81240932f77dc5da4c165f4acb2cc5e8b6f389a1
Jon Schulte <jonlouis.schulte@agentur-david.de>
2025-10-10 13:27:50 +0200

    fix: update Vite config to use mode for base URL and adjust GitHub Actions build command

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
     vueDevTools(),
   ],
-  base: process.env.NODE_ENV === 'production' ? '/homepage/' : '/',
+  base: mode === 'production' ? '/homepage/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+}))
