5ee368c738cc73f27afc2a8f8732165b6a7b8ddf
Jon Schulte <jonlouis.schulte@agentur-david.de>
2025-10-10 13:22:18 +0200

    fix: update favicon path in index.html and set base URL in vite.config.js for production

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: process.env.NODE_ENV === 'production' ? '/homepage/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
