272935a7dc55efd5b0928aef983091b627aeaada
Jon Schulte <jonlouis.schulte@agentur-david.de>
2025-10-15 18:22:17 +0200

    feat: add Contact component with email and social media links

---
+++ src/components/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="contact field-row-stacked" >
+        <a href="mailto:hi@jon3000.cool">Email: hi@jon3000.cool</a>
+        <a href="https://github.com/kullup">GitHub: kullup</a>
+        <a href="https://www.linkedin.com/in/jon-schulte-04219825a/">LinkedIn</a>
+    </div>
+</template>
+
+<style scoped>
+    .contact {
+        width: 100%; 
+        height: 100%; 
+        display: flex; 
+        flex-direction: column; 
+        justify-content: center; 
+        align-items: center;
+    }
+</style>
